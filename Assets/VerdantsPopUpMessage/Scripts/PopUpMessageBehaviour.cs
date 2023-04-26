bd77b54bb89cf2871f4df53710bdcfdb1c3775c5
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-04-26 19:48:14 +0300

    Implementing Behaviour
    Implementing code and attaching it to the prefab object

--- Assets/VerdantsPopUpMessage/Scripts/PopUpMessageBehaviour.cs
+++ Assets/VerdantsPopUpMessage/Scripts/PopUpMessageBehaviour.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PopUpMessageBehaviour : MonoBehaviour
 {
+    private TMP_Text text;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        text = GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        text.color -= new Color(0, 0, 0, Time.deltaTime / 2);
+
+        text.transform.position += new Vector3(0,Time.deltaTime / 2,0);
+
+        if (text.color.a <= 0)
+            Destroy(gameObject);
     }
 }
