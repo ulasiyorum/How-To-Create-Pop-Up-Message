7d9f644be5f92538101a4cef66d11cdd68fdbf06
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-04-26 19:57:28 +0300

    Implement Message(content, color)

--- Assets/VerdantsPopUpMessage/Scripts/StartPopUpMessage.cs
+++ Assets/VerdantsPopUpMessage/Scripts/StartPopUpMessage.cs
@@ -1,9 +1,13 @@
+using TMPro;
 using UnityEngine;
 
 public static class StartPopUpMessage
 {
     public static void Message(string message, Color messageColor)
     {
-        
+        GameObject go = GameObject.Instantiate(AssetManager.Instance.prefab,AssetManager.Instance.canvas);
+
+        go.GetComponent<TMP_Text>().text = message;
+        go.GetComponent<TMP_Text>().color = messageColor;
     }
 }
